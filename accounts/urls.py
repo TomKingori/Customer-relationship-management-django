b94b7223cbaebf96ab2d030e4cbf6e91e8c89e27
TomKingori <tomkingori66@gmail.com>
2022-11-07 15:46:08 +0300

    rendering data and url routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
     
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/', views.customers),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customers/<str:pk>', views.customers, name="customer"),
 ]
